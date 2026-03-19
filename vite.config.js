d6f6d0ef7f201f11e488235861ad6b6ae3c048cb
Danilo Rincón <rinconsicologico@gmail.com>
2026-03-19 15:55:24 -0500

    fix: force cache busting on build

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash]-${Date.now()}.js`,
+        chunkFileNames: `assets/[name]-[hash]-${Date.now()}.js`,
+      }
+    }
+  }
 })
